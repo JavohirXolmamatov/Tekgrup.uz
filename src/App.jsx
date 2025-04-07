3f39be1fe64ae89b15ff8eb67b0efd9665e1b16f
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-04-07 23:49:06 +0500

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { Routes, Route } from "react-router";
-import { Main } from "./components";
+import { Routes, Route, useLocation } from "react-router";
+import { About, Contact, Footer, Header, Main, News, Products } from "./pages";
 
 function App() {
+  const location = useLocation();
+
+  switch (location.pathname) {
+    case "/":
+      document.title = "TEKGRUP";
+      break;
+    case "/news":
+      document.title = "News - TEKGRUP";
+      break;
+    case "/product":
+      document.title = "Product - TEKGRUP";
+      break;
+    case "/about":
+      document.title = "About - TEKGRUP";
+      break;
+    case "/contact":
+      document.title = "Contact - TEKGRUP";
+      break;
+    default:
+      document.title = "TEKGRUP";
+      break;
+  }
   return (
     <div>
+      <header>
+        <Header />
+      </header>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
