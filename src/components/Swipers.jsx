3a3c6e9f7640f7663a3de04f7ae09359c1718de9
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-04-08 23:15:02 +0500

    Main section mobile design

--- src/components/Swipers.jsx
+++ src/components/Swipers.jsx
@@ -13,43 +13,57 @@ const Swipers = () => {
     <>
       <Swiper
         slidesPerView={3}
-        centeredSlides={true}
         spaceBetween={80}
+        centeredSlides={true}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
         loop={true}
         modules={[Pagination, Navigation, Autoplay]}
-        className="mySwiper"
+        className="mySwiper mb-5"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+          },
+        }}
       >
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={onko} alt="onko" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={pf} alt="pf" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={kopas} alt="kopas" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={atabay} alt="atabay" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={astella} alt="astella" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[250px] w-[400px] bg-white flex items-center justify-center align-middle">
+          <div className="md:h-[250px] h-[200px] md:w-[400px] w-full bg-white flex items-center justify-center align-middle">
             <img src={dalin} alt="dalin" className="w-[230px] h-[130px]" />
           </div>
         </SwiperSlide>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,8 +7,12 @@ function Main() {
     <div className="w-full mb-10">
       {/* 1 */}
       <section className="relative">
-        <img src={bg1} alt="bg1" className="w-full md:h-[650px] object-cover" />
-        <div className="absolute w-full md:h-[650px] top-0 left-0 right-0 bg-blue-800/30 flex flex-col items-center justify-center align-middle gap-y-6">
+        <img
+          src={bg1}
+          alt="bg1"
+          className="w-full md:h-[650px] h-[400px] object-cover"
+        />
+        <div className="absolute w-full md:h-[650px] h-[400px] top-0 left-0 right-0 bg-blue-800/30 flex flex-col items-center justify-center align-middle gap-y-6">
           <span
             data-aos="zoom-in"
             className="uppercase bg-white py-2 px-6 rounded-2xl font-medium text-[#009F7F] tracking-widest"
@@ -17,11 +21,11 @@ function Main() {
           </span>
           <h1
             data-aos="zoom-in"
-            className="text-6xl text-center font-bold md:w-[50%] leading-16 text-white"
+            className="md:text-6xl text-3xl text-center font-bold md:w-[50%] md:leading-16 text-white"
           >
             Sog‘lom dunyo uchun harakat qilamiz!
           </h1>
-          <p data-aos="zoom-in" className="text-white/90 text-lg">
+          <p data-aos="zoom-in" className="text-white/90 text-lg text-center">
             Biz bilan, faqat dori-darmon emas, balki keng qamrovli xizmatlarni
             ham kashf eting.
           </p>
@@ -39,61 +43,61 @@ function Main() {
       <section className="w-full py-25 bg-blue-500/5">
         <div
           data-aos="zoom-in-down"
-          className="w-8/10 mx-auto flex md:flex-col gap-10 bg-white p-10 rounded-2xl shadow-2xl"
+          className="md:w-8/10 w-9/10 mx-auto flex md:flex-col flex-row md:gap-10 gap-5 bg-white p-10 rounded-2xl shadow-2xl"
         >
-          <div className="flex w-full">
+          <div className="flex md:flex-row flex-col gap-5">
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={8} duration={2} />}+ yil
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
             </div>
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={10} duration={2} />}+
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
             </div>
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={30} duration={2} />}+
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
             </div>
           </div>
-          <div className="flex w-full">
+          <div className="flex md:flex-row flex-col gap-5">
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={15} duration={2} />}+
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
             </div>
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={200} duration={2} />}+
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
             </div>
             <div
               data-aos="zoom-in-down"
-              className="text-center w-1/3 text-[#0C4DA2]"
+              className="text-center md:w-1/3 w-full text-[#0C4DA2]"
             >
-              <h1 className="text-5xl font-bold pb-2">
+              <h1 className="md:text-5xl text-2xl font-bold pb-2">
                 {<CountUp end={2000} duration={1} />}kv
               </h1>
               <span className="font-medium text-md">Bozorda mavjud</span>
@@ -104,11 +108,15 @@ function Main() {
 
       {/* 3 */}
       <section className="relative">
-        <img src={bg2} alt="bg1" className="w-full md:h-[650px] object-cover" />
-        <div className="absolute w-full md:h-[650px] top-0 left-0 right-0 bg-blue-800/30 flex flex-col items-center justify-center align-middle gap-y-6">
+        <img
+          src={bg2}
+          alt="bg1"
+          className="w-full md:h-[650px] h-[400px] object-cover"
+        />
+        <div className="absolute w-full md:h-[650px] h-[400px] top-0 left-0 right-0 bg-blue-800/30 flex flex-col items-center justify-center align-middle gap-y-6">
           <h1
             data-aos="zoom-in"
-            className="text-5xl text-center font-bold md:w-[55%] leading-16 text-white"
+            className="md:text-5xl text-3xl text-center font-bold md:w-[55%] md:leading-16 text-white"
           >
             Farmasevtika industriyasida yetakchi distribyutorlardan birimiz
           </h1>
@@ -127,28 +135,28 @@ function Main() {
 
       {/* 4 */}
       <section className="w-full bg-blue-500/5 h-auto py-10">
-        <div className="md:w-8/10 mx-auto py-20">
+        <div className="md:w-8/10 w-9/10 mx-auto md:py-20 py-5">
           <h1
             data-aos="fade-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine"
-            className="uppercase text-[#009F7F] font-medium"
+            className="uppercase text-[#009F7F] font-medium md:text-start text-center"
           >
             Nega Bizni tanlashadi
           </h1>
-          <div className="flex gap-x-5 items-center align-middle">
+          <div className="flex md:flex-row flex-col gap-x-5 items-center align-middle">
             <div
               data-aos="fade-right"
               data-aos-offset="300"
               data-aos-easing="ease-in-sine"
-              className="w-2/4"
+              className="md:w-2/4 w-full"
             >
-              <h1 className="text-5xl font-medium mt-3">
+              <h1 className="text-5xl font-medium mt-3 md:text-start text-center">
                 Eng yaxshi mijozlar uchun eng yaxshi xizmatlar.
               </h1>
               <img src={testimonial} alt="testimonial" />
             </div>
-            <div className="w-2/4 flex flex-col gap-5">
+            <div className="md:w-2/4 w-full flex flex-col gap-5">
               <div
                 data-aos="fade-left"
                 data-aos-offset="300"
@@ -186,7 +194,7 @@ function Main() {
         <img
           src={medicine}
           alt="bg1"
-          className="w-full md:h-[650px] object-cover"
+          className="w-full md:h-[650px] h-[500px] object-cover"
         />
         <div className="absolute w-full md:h-[650px] top-0 left-0 right-0 bg-blue-800/70 flex flex-col items-center justify-center align-middle gap-y-6">
           <h1
